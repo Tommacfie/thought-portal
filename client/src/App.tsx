de5e9e9f166a6d8c5e40cfaa49d6ecb7b3a96d9b
Tom Macfie <tommacfie@aol.com>
2022-10-15 19:13:12 +0200

    feat: inputs and text area
    Signed-off-by: Tom Macfie <tommacfie@aol.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div>Programming</div>;
+  return (
+    <div>
+      <h1>Thought Portal</h1>
+      <input name="title" placeholder="Title..." />
+      <textarea placeholder="Your thoughts here..." />
+      <input name="tags" placeholder="tags" />
+    </div>
+  );
 };
 
 export default App;
