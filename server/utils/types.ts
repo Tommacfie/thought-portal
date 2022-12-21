58166877d70d6021779c3bc8a21fa66ed0ce2d97
Tom Macfie <tommacfie@aol.com>
2022-12-21 20:51:13 +0100

    fix: add _ids to types/models

--- server/utils/types.ts
+++ server/utils/types.ts
@@ -1,15 +1,18 @@
 export type UserType = {
+  _id: string;
   name: string;
   email: string;
 };
 
 export type ThoughtType = {
+  _id: string;
   title: string;
   body: string;
   tags: string[];
 };
 
 export type MoodType = {
+  _id: string;
   name: string;
   description: [string];
   intensity: {
@@ -20,6 +23,7 @@ export type MoodType = {
 };
 
 export type ThoughtRecordType = {
+  _id: string;
   title: string;
   situation: string;
   moods: [MoodType];
