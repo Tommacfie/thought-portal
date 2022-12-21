1dc5e3892fceadd5856aa2ddd253f2f26af3189f
Tom Macfie <tommacfie@aol.com>
2022-12-21 20:51:54 +0100

    feat: getthoughtrecord and updatethoughtrecord

--- server/controllers/thoughtRecordController.ts
+++ server/controllers/thoughtRecordController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Mood from '../models/Mood';
 import ThoughtRecord from '../models/thoughtRecord';
+import { MoodType } from '../utils/types';
 
 export const getThoughtRecords = async (_req: Request, res: Response) => {
   try {
@@ -11,7 +12,7 @@ export const getThoughtRecords = async (_req: Request, res: Response) => {
     res.send(error).status(500);
   }
 };
-export const getThoughtRecordById = async (req: Request, res: Response) => {
+export const getThoughtRecord = async (req: Request, res: Response) => {
   try {
     const thoughtRecord = await ThoughtRecord.find({
       _id: req.query.id,
@@ -28,6 +29,39 @@ export const createThoughtRecord = async (req: Request, res: Response) => {
     const newMood = await Mood.create(req.body.moods);
     req.body.moods = newMood;
     const newThoughtRecord = await ThoughtRecord.create(req.body);
+    res.send(newThoughtRecord).status(201);
+  } catch (error) {
+    console.log('error:', error);
+    res.send(error).status(500);
+  }
+};
+
+export const updateThoughtRecord = async (req: Request, res: Response) => {
+  const moods: MoodType[] = req.body.moods;
+
+  try {
+    const updatedMoods = await Promise.all(
+      moods.map(async (mood, index) => {
+        if (mood._id) {
+          const updatedMood = await Mood.findOneAndUpdate(
+            { _id: mood },
+            { ...req.body.moods[index] },
+            { new: true }
+          );
+          return updatedMood;
+        } else {
+          const newMood = await Mood.create(req.body.moods[index]);
+          return newMood;
+        }
+      })
+    );
+    const newThoughtRecord = await ThoughtRecord.findOneAndUpdate(
+      {
+        _id: req.body._id,
+      },
+      { ...req.body, moods: updatedMoods },
+      { new: true }
+    );
     res.send(newThoughtRecord).status(201);
   } catch (error) {
     console.log('error:', error);
