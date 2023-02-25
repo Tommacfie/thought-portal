585be0b6e79142c522d0f143f8d1089b0ea83641
Tommacfie <tommacfie@aol.com>
2023-02-25 10:58:21 +0100

    chore: use react-hook-forms
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- client/src/pages/journal/components/JournalEntryForm.tsx
+++ client/src/pages/journal/components/JournalEntryForm.tsx
@@ -1,80 +1,61 @@
 import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { postJournalEntry } from '../../../services/apiService';
 import { CreateJournalEntryType } from '../../../types/types';
-import { compileTags } from '../helpers/compileTags';
 import { writeJournalEntryToFile } from '../helpers/writeJournalEntryToFile';
 
-const initialValues: CreateJournalEntryType = {
-  title: '',
-  journalEntry: '',
-  tags: [],
-};
-
 const JournalEntryForm = () => {
-  const [journalEntry, setJournalEntry] =
-    useState<CreateJournalEntryType>(initialValues);
+  const { register, handleSubmit } = useForm<CreateJournalEntryType>();
+  const navigate = useNavigate();
+
+  const submissionHandler: SubmitHandler<CreateJournalEntryType> = async (
+    data
+  ) => {
+    await postJournalEntry(data);
+    if (saveCopy) {
+      writeJournalEntryToFile(data);
+    }
+    navigate('/journalEntries');
+  };
+
   const [saveCopy, setSaveCopy] = useState(false);
   const [readOnly] = useState(false);
   return (
     <form
       className="flex flex-col justify-evenly items-center border py-10 h-1/2"
-      onSubmit={async (event) => {
-        await postJournalEntry(journalEntry);
-        if (saveCopy) {
-          writeJournalEntryToFile(journalEntry);
-        }
-        setJournalEntry(initialValues);
-      }}
+      onSubmit={handleSubmit(submissionHandler)}
     >
       <div className="text-center text-xl">Create a New Journal Entry</div>
       <label className="hidden" htmlFor="journal-entry-title-input">
         Journal Entry Title Input
       </label>
       <input
-        id="journal-entry-title-input"
         placeholder="Give your Journal Entry a title..."
         className="border m-1 p-1 w-1/2 text-center"
-        value={journalEntry.title}
-        onChange={(event) => {
-          setJournalEntry((prev) => {
-            return { ...prev, title: event.target.value };
-          });
-        }}
+        {...register('title')}
+        name={'title'}
         readOnly={readOnly}
       />
       <label className="hidden" htmlFor="journal-entry-body-input">
         Journal Entry Body Input
       </label>
       <textarea
-        id="journal-entry-body-input"
         placeholder="Write your thoughts here..."
         className="m-1 border px-3 py-2 w-3/4"
         rows={15}
-        value={journalEntry.journalEntry}
-        onChange={(event) => {
-          setJournalEntry((prev) => {
-            return { ...prev, journalEntry: event.target.value };
-          });
-        }}
+        {...register('journalEntry')}
+        name={'journalEntry'}
         readOnly={readOnly}
       />
       <label className="hidden" htmlFor="journal-entry-tags-input">
         Journal Entry Tags Input
       </label>
       <input
-        id="journal-entry-tags-input"
         placeholder="Add comma-separated tags here, i.e: work, friends, parents etc"
-        value={compileTags(journalEntry.tags)}
         className="border w-2/3 py-1 px-2 m-1 text-sm"
-        onChange={(event) => {
-          const newTags = event.target.value.split(',');
-          setJournalEntry((prev) => {
-            return {
-              ...prev,
-              tags: [...newTags],
-            };
-          });
-        }}
+        {...register('tags')}
+        name={'tags'}
         readOnly={readOnly}
       />
       <div className="flex w-full justify-evenly items-center">
@@ -85,7 +66,6 @@ const JournalEntryForm = () => {
           <input
             type="checkbox"
             className="m-1"
-            id="save-journal-entry-copy-checkbox"
             onChange={() => setSaveCopy(!saveCopy)}
           />
           <label className="m-1" htmlFor="save-journal-entry-checkbox">
