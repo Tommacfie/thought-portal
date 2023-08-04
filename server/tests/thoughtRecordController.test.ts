a52499a19152872bf725a589f47317468d243c6a
Tom Macfie <tm@paceteq.com>
2023-08-04 19:52:01 +0200

    test: change test filename and add test for thoughtrecord not found
    Signed-off-by: Tom Macfie <tm@paceteq.com>

---
+++ server/tests/thoughtRecordController.test.ts
@@ -0,0 +1,105 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../index';
+import { DB } from '../config';
+
+beforeAll(async () => {
+  await mongoose.connect(`${DB}`);
+});
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+const newThoughtRecord = {
+  title: 'Test Thought Record',
+  situation: 'Testing environment',
+  moods: [],
+  automaticThoughts: [],
+  evidenceFor: [],
+  evidenceAgainst: [],
+  alternativeThoughts: [],
+};
+const editedThoughtRecord = {
+  title: 'Edit Thought Record',
+  situation: 'Edit Testing environment',
+  moods: [],
+  automaticThoughts: [],
+  evidenceFor: [],
+  evidenceAgainst: [],
+  alternativeThoughts: [],
+};
+
+let thoughtRecordID: string;
+describe('Thought Record Controller', () => {
+  it('should return all thought records', async () => {
+    const response = await request(app).get('/thoughtRecords');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+  it('should allow a user to create a thought record', async () => {
+    const response = await request(app)
+      .post('/thoughtRecord')
+      .send(newThoughtRecord)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    thoughtRecordID = response.body._id;
+    expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe('Test Thought Record');
+    expect(response.body.situation).toBe('Testing environment');
+  });
+  it('should return a thought record by id', async () => {
+    const response = await request(app).get(
+      `/thoughtRecord?id=${thoughtRecordID}`
+    );
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe('Test Thought Record');
+    expect(response.body.situation).toBe('Testing environment');
+  });
+  it("should return a 404 error if thought record doesn't exist", async () => {
+    const response = await request(app).get(
+      `/thoughtRecord?id=64aaaaaaaaaa08a07774130b`
+    );
+    expect(response.statusCode).toBe(404);
+    expect(response.text).toBe('Thought Record not found');
+  });
+  it('should allow a user to edit a thought record', async () => {
+    const createThoughtRecordToEdit = await request(app)
+      .post('/thoughtRecord')
+      .send(newThoughtRecord)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    const createdThoughtRecordToEditId = createThoughtRecordToEdit.body._id;
+
+    const editThoughtRecord = await request(app)
+      .put('/thoughtRecord')
+      .query({ id: createdThoughtRecordToEditId })
+      .send(editedThoughtRecord)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    expect(editThoughtRecord.statusCode).toBe(200);
+    expect(editThoughtRecord.body.title).toBe('Edit Thought Record');
+    expect(editThoughtRecord.body.situation).toBe('Edit Testing environment');
+  });
+  it('should allow a user to delete a thought record', async () => {
+    const createThoughtRecordToDelete = await request(app)
+      .post('/thoughtRecord')
+      .send(newThoughtRecord)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    const createdThoughtRecordToDeleteId = createThoughtRecordToDelete.body._id;
+
+    const deleteThoughtRecord = await request(app)
+      .delete('/thoughtRecord')
+      .query({ id: createdThoughtRecordToDeleteId })
+      .send(editedThoughtRecord)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+
+    expect(deleteThoughtRecord.statusCode).toBe(200);
+    expect(deleteThoughtRecord.body.title).toBe('Test Thought Record');
+  });
+});
