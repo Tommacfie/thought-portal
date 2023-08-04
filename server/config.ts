8c3dd44351d5b16ec481a2d41f6b87916782598e
Tom Macfie <tm@paceteq.com>
2023-08-04 19:20:29 +0200

    chore: cleanup node env variables and logs for test environment
    Signed-off-by: Tom Macfie <tm@paceteq.com>

--- server/config.ts
+++ server/config.ts
@@ -4,4 +4,3 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const DB = process.env.DB;
-export const ISTEST = process.env.ISTEST;
--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import router from './router';
 import cors from 'cors';
 
-import { PORT, ISTEST } from './config';
+import { PORT } from './config';
 
 export const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-if (!ISTEST) {
+if (process.env.NODE_ENV != 'TEST') {
   app
     .listen(PORT, () => {
       // eslint-disable-next-line no-console
